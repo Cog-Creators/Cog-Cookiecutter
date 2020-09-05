59ca78ae30d96aff08416da7ba65d37cb5daf1ef
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-09-05 19:52:38 +0100

    lets use a simplified version of this.

--- {{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.package_name}}/__init__.py
@@ -5,8 +5,7 @@ from redbot.core.bot import Red
 
 from .{{cookiecutter.package_name}} import {{cookiecutter.cog_class_name}}
 
-with open(Path(__file__).parent / "info.json") as fp:
-    __red_end_user_data_statement__ = json.load(fp)["end_user_data_statement"]
+__red_end_user_data_statement__ = "{{ cookiecutter.end_user_data_statement }}"
 
 
 async def setup(bot: Red) -> None:
