bd6ce3619c60b330d4734000216984f4fe30b2a6
Reinis Mednis <mednisreinis@gmail.com>
2023-07-13 11:58:40 +0300

    Add `await` to `bot.add_cog()` call (#5)

--- {{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.package_name}}/__init__.py
@@ -10,4 +10,4 @@ with open(Path(__file__).parent / "info.json") as fp:
 
 
 async def setup(bot: Red) -> None:
-    bot.add_cog({{cookiecutter.cog_class_name}}(bot))
+    await bot.add_cog({{cookiecutter.cog_class_name}}(bot))
